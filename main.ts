4d1f45f787db6f707e0c0632909a7a860ab77c59
Lightmaxifrvr <100568192+Lightmaxifrvr@users.noreply.github.com>
2025-10-27 12:14:27 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,12 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    WaiterWaitermorewaitplease = 1
-    for (let index = 0; index < 4; index++) {
-        basic.showLeds(`
-            . # # # .
-            . # # # #
-            # # # # #
-            # # # # .
-            . # # # .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-    }
-})
-let WaiterWaitermorewaitplease = 0
 let LightState = 0
-WaiterWaitermorewaitplease = 0
 basic.forever(function () {
     if (LightState > 2) {
         LightState = 0
     }
     if (LightState == 0) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
         pins.digitalWritePin(DigitalPin.P1, 0)
         pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P0, 1)
     } else if (LightState == 1) {
         pins.digitalWritePin(DigitalPin.P0, 0)
         pins.digitalWritePin(DigitalPin.P1, 1)
@@ -39,8 +18,4 @@ basic.forever(function () {
     } else {
     	
     }
-})
-basic.forever(function () {
-    LightState += 1
-    basic.pause(1000)
 })
