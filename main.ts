badba0868f7a259b61d05d5dbfdd0a76d6412bee
Lightmaxifrvr <100568192+Lightmaxifrvr@users.noreply.github.com>
2025-10-27 11:55:21 +0100

    dasda

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+let LightState = 0
+basic.forever(function () {
+    if (LightState == 0) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    } else if (LightState == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    } else if (LightState == 2) {
+    	
+    } else {
+    	
+    }
+})
